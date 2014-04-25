f27993951e64bd5600521e5b524d895717c837eb
Apprentice <apprentice@dbc31.local>
2014-04-24 17:16:15 -0700

    working base version

---
+++ PlatypusX/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlatypusX::Application.config.secret_token = '0e1305abb73e32ad3f8a42279949f1d946101e3d16eb751024242ed64059869e71b73ab954828cb67281a42c31a7e963534e73b10c5cd385d1df608e96c9e346'
