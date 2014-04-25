3e7609cf413b2b3b3d578bf891b2bd0929d733e2
Apprentice <apprentice@dbc31.local>
2014-04-25 10:29:35 -0700

    now with name

--- PlatypusX/app/models/user.rb
+++ PlatypusX/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-  attr_accessible :username, :email, :password, :password_confirmation
+  attr_accessible :name, :email, :password, :password_confirmation
 
   validates_uniqueness_of :email
 
